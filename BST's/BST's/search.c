1834202de3e9e1d074d2cf3c789031aae71beccc
vincentkacperskiGit <kacperskivincent@gmail.com>
2026-03-24 20:04:08 -0400

    Developed the rest of the searchNodes() function to traverse the trees subtrees.

--- BST's/BST's/search.c
+++ BST's/BST's/search.c
@@ -13,8 +13,18 @@ char searchNodes(struct Node* ptr, char data) {
 		return NULL;
 	}
 
+	//Check if the search value has been found
+	printf("ptr->data: %c, data: %c\n", ptr->data, data);
+	if (data == ptr->data) {
+		return data;
+	}
 
-
-
-
+	if (data > ptr->data) {
+		return searchNodes(ptr->right, data); //Traverse to the left next node
+	}
+	else {
+		if (data < ptr->data) {
+			return searchNodes(ptr->left, data); //Traverse to the right next node
+		}
+	}
 }
