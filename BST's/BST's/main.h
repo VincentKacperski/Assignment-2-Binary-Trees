d824d5f5778c52d88eb68c6ac35f296fa0e50c95
vincentkacperskiGit <kacperskivincent@gmail.com>
2026-03-24 21:14:44 -0400

    Included function prototypes for all c files.

--- BST's/BST's/main.h
+++ BST's/BST's/main.h
@@ -7,3 +7,7 @@ struct Node {
 
 //Program header files
 struct Node* insertNodes(struct Node* ptr, char data);
+int determineHeight(struct Node* rootNode);
+char searchNodes(struct Node* newPtr, char data);
+int printTraversal(struct Node* rootNode);
+int countNodes(struct Node* ptr);
